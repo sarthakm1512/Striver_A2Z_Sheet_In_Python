d44a30729d024939a7b9af29f1338f8451e9058e
Sarthak Malviya <120418085+sarthakm1512@users.noreply.github.com>
2025-06-13 10:49:53 +0530

    Update 01_UserInputOutput.py
    User Input/Output solution code added

--- Step 1: Learn the Basics/Lec 1: Things to know in Python or any language/01_UserInputOutput.py
+++ Step 1: Learn the Basics/Lec 1: Things to know in Python or any language/01_UserInputOutput.py
@@ -1,7 +1,9 @@
 # Question Link: https://www.naukri.com/code360/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf%5C
 """
 Find Character Case 
+
 Problem Statement : Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
+
 1, if the character is an uppercase alphabet (A - Z).
 0, if the character is a lowercase alphabet (a - z).
 -1, if the character is not an alphabet.
