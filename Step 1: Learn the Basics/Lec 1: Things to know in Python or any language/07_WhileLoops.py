9d926a376ac7fa1bba862337270426faa2054606
Sarthak Malviya <120418085+sarthakm1512@users.noreply.github.com>
2025-06-25 14:27:22 +0530

    While Loops solution code added

--- Step 1: Learn the Basics/Lec 1: Things to know in Python or any language/07_WhileLoops.py
+++ Step 1: Learn the Basics/Lec 1: Things to know in Python or any language/07_WhileLoops.py
@@ -1,6 +1,6 @@
-#Question Link: https://www.geeksforgeeks.org/problems/while-loop-printtable-java/1
+#Question Link: https://www.geeksforgeeks.org/problems/while-loop-printtable-java/1  
 """
-While loop- printTable
+While loop- PrintTable
 
 Problem Statement: While loop is another loop like for loop but unlike for loop it only checks for one condition.
 Here, we will use a while loop and print a number n's table in reverse order.
