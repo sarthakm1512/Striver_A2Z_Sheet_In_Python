7ffd82c4221610dc758a98aa3dccbcb01199ec80
Sarthak Malviya <120418085+sarthakm1512@users.noreply.github.com>
2025-06-25 14:28:37 +0530

    Functions (Pass By Reference & Value) solution code added

--- Step 1: Learn the Basics/Lec 1: Things to know in Python or any language/08_FunctionsPassByReference&Value.py
+++ Step 1: Learn the Basics/Lec 1: Things to know in Python or any language/08_FunctionsPassByReference&Value.py
@@ -3,7 +3,8 @@
 Pass by Reference and Value
 
 Problem Statement: Geek is learning about functions and calling a function with arguments. He learns that passing can take one of two forms: pass by value or pass by reference.
-Geek wishes to add 1 and 2, respectively, to the parameter passed by value and reference. Help Geek in fulfilling his goal.
+Geek wishes to add 1 and 2, respectively, to the parameter passed by value and reference.
+Help Geek in fulfilling his goal.
 """
 
 class Solution:
